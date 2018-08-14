d6e43109ca4710a478fbea0e38e39f689d83abca
Diego Vidal Almazan Orozco <dvalmazan@scio.local>
2018-08-14 11:30:14 -0500

    Subiendo AppContactos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { Contacts, ContactFieldType, IContactFindOptions } from '@ionic-native/contacts';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  ourtype: ContactFieldType[]=["displayName"];
+  avatar:string="./assets/icon/avatar.png";
+  contactos = [];
 
+  constructor(public navCtrl: NavController, private contacts:Contacts) {
+    this.viewContacts('');
+  }
+
+  async viewContacts(q) {
+    try {
+      const option: IContactFindOptions = {
+        filter: q
+      }
+      this.contacts.find(this.ourtype, option).then(conta => {
+      this.contactos = conta;
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  onKeyUp(ev){
+    this.viewContacts(ev.target.value);
   }
 
 }
